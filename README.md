4ee4656a8fa3bbbd6d702c9b4bdd9d3f453d9f25
Tim Church <tim.church+github@gmail.com>
2012-01-31 18:18:46 +0100

    almost ready to release

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 InstaList
-=========
+---------
 
-*InstaList* is a simple, interactive web application for creating printable checklists in the browser.
+*InstaList* is a little checklist making tool that allows you to quickly create custom, printer-friendly checklists from a web browser. Great for quick to-do lists and grocery lists.
+
+
 
 Features
 --------
 
-* Printer friendly
+* Printer friendly checklists
 * Supports nested lists (up to 3 levels deep)
 * Drag & drop to re-order and to change nesting level
 * Click any text to edit
@@ -18,6 +20,7 @@ Credits
 
 * [Twitter Bootstrap Framework](http://twitter.github.com/bootstrap/)
 * [jQuery](http://jquery.com/)
+* [jQuery UI](http://jqueryui.com/)
 * [nestedSortable jQuery plugin](https://github.com/mjsarfatti/nestedSortable)
 * Icons from [Glyphicons](http://glyphicons.com/)
 
@@ -25,3 +28,5 @@ License
 -------
 
 [Creative Commons Attribution-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0/)
+
+Copyright 2012 Tim Church
--- js/instalist.js
+++ js/instalist.js
@@ -1,36 +1,51 @@
 $(document).ready(function() {
+	// Enable checkboxes
 	$(".box").live('click', function(event) {
 		$(this).toggleClass("checked");
 	});
 
+	// Enable text editing on list items
 	$("div.value").live('click', function(event) {
 		var text = $(this).text();
-		$(this).next('input').attr('value', text).show().focus();
+		$(this).next('input').attr('value', text).show().focus().select();
 		$(this).hide();
 	});
-
 	$('input.value').live('blur', function(event) {
 		var text = $(this).attr('value');
 		if (text) {
-			$(this).prev('.value').text(text).show();
+			$(this).prev('div.value').text(text).show();
 			$(this).hide();	
 		} else {
-			$(this).parents('li').remove();
+			// DELETE row if text is empty
+			var sublist = $(this).parents('li').first().children('ol');
+			if(sublist.length > 0) {
+				var ol = sublist[0];
+				var sublist_items = $(ol).children('li');
+				if(sublist_items.length > 0) {
+					$(this).prev('div.value').text("").show();
+					$(this).hide();
+				} else {
+					$(this).parents('li').first().remove();
+				}
+			} else {
+				$(this).parents('li').first().remove();
+			}
 		}
 	}).click();
 
+	// Enable title editing
 	$("#list-title").click(function(event) {
 		var title = $(this).text();
-		$('#list-title-edit').attr('value', title).show().focus();
+		$('#list-title-edit').attr('value', title).show().focus().select();
 		$(this).hide();
 	});
-
 	$('#list-title-edit').focusout(function(event) {
 		var title = $(this).attr('value');
 		$('#list-title').text(title).show();
 		$(this).hide();
 	});
 	
+	// Enable drag & drop sorting
 	$("#checklist").nestedSortable({
 			disableNesting: 'no-nest',
 			forcePlaceholderSize: true,
@@ -40,24 +55,55 @@ $(document).ready(function() {
 			maxLevels: 3,
 			opacity: .6,
 			placeholder: 'sortable-placeholder',
-			revert: 250,
-			tabSize: 35,
+			revert: false,
+			tabSize: 34,
 			tolerance: 'pointer',
 			toleranceElement: '> div'
 	});
-	$( "#checklist" ).disableSelection();
+	$("#checklist").disableSelection();
 
+	// Enable "Add item" button
 	$("#add").click(function(event) {
-		$('#checklist').append($('<li>')
-					   .append($('<div>').attr('class', 'row')
-					   .append($('<div>').attr('class', 'handle-container')
-					   .append($('<img>').attr('src', "images/glyphicons_186_move.png").attr('class', 'handle')))
-					   .append($('<div>').attr('class', 'box'))
-					   .append($('<div>').attr('class', 'value').text("New Item"))
-					   .append($('<input>').attr("type", "text").attr('class', 'value editMode'))));
+		add_row();
+		$('#checklist li:last-child div.value').click();
+	});
+	
+	// Enable buttons on reset confirmation modal
+	$("#confirm-reset").click(function(event) {
+		$("#reset-confirm-modal").modal('hide');
+		reset();
+		$('#checklist li:last-child div.value').click();
+	});
+	$("#cancel-reset").click(function(event) {
+		$("#reset-confirm-modal").modal('hide')
+	});
+	
+	// Enable 'About' link
+	$("#show-about").click(function(event) {
+		$("#about").toggle();
+	});
+	$("#close-about").click(function(event) {
+		$("#about").hide();
 	});
 
+	// Enable print button
 	$("#print").click(function(){
 		window.print();
 	});
 });
+
+function add_row() {
+	$('#checklist').append($('<li>')
+				   .append($('<div>').attr('class', 'row')
+				   .append($('<div>').attr('class', 'handle-container')
+				   .append($('<img>').attr('src', "images/glyphicons_186_move.png").attr('class', 'handle')))
+				   .append($('<div>').attr('class', 'box'))
+				   .append($('<div>').attr('class', 'value').text("New Item"))
+				   .append($('<input>').attr("type", "text").attr('class', 'value editMode'))));
+}
+
+function reset() {
+	$("#list-title").text("List Title");
+	$("#checklist").children().remove();
+	add_row();
+}
